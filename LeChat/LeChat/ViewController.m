9b5b7465c8bed0252b07cb59a8757b929da482fb
baojizi <baojizi@wacai.com>
2017-02-13 14:05:41 +0800

    初始化功能，集合react native

--- LeChat/LeChat/ViewController.m
+++ LeChat/LeChat/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <React/RCTRootView.h>
 
 @interface ViewController ()
 
@@ -17,6 +18,24 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor = [UIColor greenColor];
+
+    
+    NSURL *jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle"];
+    // For production use, this `NSURL` could instead point to a pre-bundled file on disk:
+    //
+    //   NSURL *jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+    //
+    // To generate that file, run the curl command and add the output to your main Xcode build target:
+    //
+    //   curl http://localhost:8081/index.ios.bundle -o main.jsbundle
+    RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
+                                                        moduleName: @"LeChat"
+                                                 initialProperties:nil
+                                                     launchOptions:nil];
+    rootView.frame = self.view.frame;
+    [self.view addSubview:rootView];
 }
 
 
